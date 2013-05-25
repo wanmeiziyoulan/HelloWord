a9af55d85de51862b0c5f149f0af10e9a540fd8c
664335363 <664335363@qq.com>
2013-05-25 17:11:28 +0800

    first upLoad project

---
+++ src/com/zdsoft/test/HelloWord.java
@@ -0,0 +1,19 @@
+/**
+* @ClassName: HelloWord
+* @Description: 
+* @author YuYang
+* @date 2013-5-25 下午5:02:08
+*/
+package com.zdsoft.test;
+
+public class HelloWord {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
